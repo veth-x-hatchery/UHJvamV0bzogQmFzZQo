253a0333bbafd9f0e11ff094bd39c5abe2be341b
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-14 11:01:50 -0300

    Sign In Error Message

--- lib/features/signin/presentation/bloc/sign_in_state.dart
+++ lib/features/signin/presentation/bloc/sign_in_state.dart
@@ -2,7 +2,6 @@ part of 'sign_in_bloc.dart';
 
 abstract class SignInState extends Equatable {
   const SignInState();
-
   @override
   List<Object> get props => [];
 }
@@ -15,7 +14,12 @@ class SignInDenied extends SignInState {}
 
 class SignInCancelled extends SignInState {}
 
-class SignInError extends SignInState {}
+class SignInError extends SignInState {
+  final String message;
+  const SignInError({required this.message});
+  @override
+  List<Object> get props => [message];
+}
 
 class SignInEmailWithResetPasswordInstructionsSent extends SignInState {}
 
