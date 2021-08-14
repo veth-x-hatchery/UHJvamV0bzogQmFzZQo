b9ae35d4cf65d7db39c65b2ecbdd9f0ea424a92b
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-14 10:49:33 -0300

    Studying implement custom validators

---
+++ test/features/signin/presentation/utils/input_converter_test.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:vethx_login/features/signin/presentation/utils/input_converter.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInt', () {
+    test(
+      'should return an integer when the string represents an unsigned integer',
+      () async {
+        // arrange
+        const str = '123';
+        // act
+        final result = inputConverter.stringToUnsignedInteger(str);
+        // assert
+        expect(result, Right(123));
+      },
+    );
+  });
+}
