1203802385aa5b67c66eb278df2a7d6a80a910a5
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-14 19:19:43 -0300

    Simplifying on refactoring

--- lib/features/signin/presentation/bloc/sign_in_bloc.dart
+++ lib/features/signin/presentation/bloc/sign_in_bloc.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:vethx_login/core/error/failures.dart';
 import 'package:vethx_login/features/signin/domain/usecases/sign_in_check_email.dart'
     as a;
 import 'package:vethx_login/features/signin/domain/usecases/sign_in_with_email_and_password.dart';
@@ -22,25 +24,21 @@ class SignInBloc extends Bloc<SignInEvent, SignInState> {
   }) : super(SignInInitial());
 
   @override
-  Stream<SignInState> mapEventToState(
-    SignInEvent event,
-  ) async* {
+  Stream<SignInState> mapEventToState(SignInEvent event) async* {
     if (event is SignInCheckEmail) {
-      yield SignInLoading();
-
-      final useCaseReturn =
-          await signInCheckIfEmailIsInUse.call(a.Params(email: event.email));
-
-      yield* useCaseReturn.fold(
-        (failure) async* {
-          yield failure.message == null
-              ? SignInError()
-              : SignInError(message: failure.message!);
-        },
-        (alreadyRegistered) async* {
-          yield alreadyRegistered ? EmailAlreadyRegistered() : EmailNotFound();
-        },
-      );
+      yield* _signInCheckEmail(
+          await signInCheckIfEmailIsInUse.call(a.Params(email: event.email)));
     }
+  }
+
+  Stream<SignInState> _signInCheckEmail(Either<Failure, bool> usecase) async* {
+    yield SignInLoading();
+    yield usecase.fold(
+      (failure) => failure.message == null
+          ? SignInError()
+          : SignInError(message: failure.message!),
+      (alreadyRegistered) =>
+          alreadyRegistered ? EmailAlreadyRegistered() : EmailNotFound(),
+    );
   }
 }
