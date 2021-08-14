75b8bc008325c0d7d09b1c74d3b5bdfbaf2a6afc
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-14 10:57:32 -0300

    Sign In Events

--- lib/features/signin/presentation/bloc/sign_in_state.dart
+++ lib/features/signin/presentation/bloc/sign_in_state.dart
@@ -2,9 +2,43 @@ part of 'sign_in_bloc.dart';
 
 abstract class SignInState extends Equatable {
   const SignInState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class SignInInitial extends SignInState {}
+
+class SignInAllowed extends SignInState {}
+
+class SignInDenied extends SignInState {}
+
+class SignInCancelled extends SignInState {}
+
+class SignInError extends SignInState {}
+
+class SignInEmailWithResetPasswordInstructionsSent extends SignInState {}
+
+class SignInGivenEmailChanged extends SignInState {}
+
+/**
+ * Event -> Sign In With Google
+ * Event -> Sign In With Facebook
+ *       State -> Allowed
+ *       State -> Denied
+ *       State -> Cancelled
+ *       State -> Error
+
+ * Event -> Sign In With Email
+ * Event -> Sign In Anonymounsly
+ *       State -> Allowed
+ *       State -> Denied
+ *       State -> Error
+
+ * Event -> Forgot my password
+ *       State -> EmailWithResetPasswordInstructionsSent
+ *       State -> Error
+ * Event -> Change the given email
+ *       State -> GivenEmailChanged
+ *       State -> Error
+ */
