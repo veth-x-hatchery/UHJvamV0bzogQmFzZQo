2ef74f370b3ea75e7f5467671d268955e0b3647e
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-14 10:36:34 -0300

    Sign In Events

--- lib/features/signin/presentation/bloc/sign_in_event.dart
+++ lib/features/signin/presentation/bloc/sign_in_event.dart
@@ -6,3 +6,50 @@ abstract class SignInEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class SignInWithGoogle extends SignInEvent {}
+
+class SignInWithFacebook extends SignInEvent {}
+
+class SignInWithEmail extends SignInEvent {
+  final String email;
+  const SignInWithEmail({
+    required this.email,
+  });
+  @override
+  List<Object> get props => [email];
+}
+
+class SignInRegister extends SignInEvent {
+  final String email;
+  final String password;
+  const SignInRegister({
+    required this.email,
+    required this.password,
+  });
+  @override
+  List<Object> get props => [
+        email,
+        password,
+      ];
+}
+
+class ForgotPassword extends SignInEvent {
+  final String email;
+  const ForgotPassword({
+    required this.email,
+  });
+  @override
+  List<Object> get props => [email];
+}
+
+class ChangeTheGivenEmail extends SignInEvent {}
+
+/**
+ * 1 - Sign In With Google
+ * 2 - Sign In With Facebook
+ * 3 - Sign In With Email
+ * 4 - Sign In Anonymounsly
+ * 5 - Forgot my password
+ * 6 - Change the given email
+ */
