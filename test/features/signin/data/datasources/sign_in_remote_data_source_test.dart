eb67a67b5847df9c7ae1be214de9f0743b09a000
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-14 10:11:47 -0300

    All tests passing

--- test/features/signin/data/datasources/sign_in_remote_data_source_test.dart
+++ test/features/signin/data/datasources/sign_in_remote_data_source_test.dart
@@ -4,40 +4,78 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:vethx_login/core/api/api.dart';
 import 'package:vethx_login/core/api/api_setup.dart';
+import 'package:vethx_login/core/error/exceptions.dart';
 import 'package:vethx_login/features/signin/data/datasources/sign_in_remote_data_source.dart';
+import 'package:vethx_login/features/signin/data/models/user_model.dart';
 
 import '../../../../fixtures/fixture_reader.dart';
 import 'sign_in_remote_data_source_test.mocks.dart';
 
 @GenerateMocks([http.Client])
 void main() {
-  late SignInRemoteSource dataSource;
-  late MockClient mockHttpClient;
-  late API api;
+  late SignInRemoteSource _dataSource;
+  late MockClient _mockHttpClient;
+  late API _api;
 
   setUp(() {
-    api = API(ApiSetupDevelopment());
-    mockHttpClient = MockClient();
-    dataSource = SignInRemoteSource(mockHttpClient, api);
+    _api = API(ApiSetupDevelopment());
+    _mockHttpClient = MockClient();
+    _dataSource = SignInRemoteSource(_mockHttpClient, _api);
   });
 
-  test(
-    'should preform a GET request on a URL with number being the endpoint and with application/json header',
-    () {
-      //arrange
-      when(mockHttpClient.get(
-        any,
-        headers: anyNamed('headers'),
-      )).thenAnswer(
-        (_) async => http.Response(fixture('sign_in_user_google.json'), 200),
-      );
-      // act
-      dataSource.currentUser();
-      // assert
-      verify(mockHttpClient.get(
-        api.endpointUri(Endpoint.signInCurrentUser),
-        headers: {'Content-Type': 'application/json'},
-      ));
-    },
-  );
+  void _setUpMockHttpClientSuccess200({Uri? uri}) {
+    when(_mockHttpClient.get(uri ?? any, headers: anyNamed('headers')))
+        .thenAnswer(
+      (_) async => http.Response(fixture('sign_in_user_google.json'), 200),
+    );
+  }
+
+  void _setUpMockHttpClientFailure404({Uri? uri}) {
+    when(_mockHttpClient.get(uri ?? any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response('Something went wrong', 404));
+  }
+
+  group('get current user', () {
+    final user = UserModel(authType: 'google', email: 'test@vethx.com');
+
+    test(
+      'should preform a GET request on a URL being the endpoint and with application/json header',
+      () {
+        //arrange
+        final uri = _api.endpointUri(Endpoint.signInCurrentUser);
+        _setUpMockHttpClientSuccess200(uri: uri);
+        // act
+        _dataSource.currentUser();
+        // assert
+        verify(_mockHttpClient.get(
+          uri,
+          headers: {'Content-Type': 'application/json'},
+        ));
+      },
+    );
+
+    test(
+      'should return NumberTrivia when the response code is 200 (success)',
+      () async {
+        // arrange
+        _setUpMockHttpClientSuccess200();
+        // act
+        final result = await _dataSource.currentUser();
+        // assert
+        expect(result, equals(user));
+      },
+    );
+
+    test(
+      'should throw a ServerException when the response code is 404 or other',
+      () async {
+        // arrange
+        _setUpMockHttpClientFailure404();
+        // act
+        final call = _dataSource.currentUser;
+        // assert
+        expect(() => call(), throwsA(TypeMatcher<ServerException>()));
+      },
+    );
+  });
 }
