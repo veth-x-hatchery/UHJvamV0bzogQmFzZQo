5f2c61634317075be66ff8a345838fde405bfb28
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-15 13:34:31 -0300

    Freezed to ValueFailure Either Fucking Thing

--- lib/features/signin/domain/entities/value_objects.dart
+++ lib/features/signin/domain/entities/value_objects.dart
@@ -1,13 +1,13 @@
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
+import 'package:vethx_login/features/signin/domain/core/failures.dart';
 
 @immutable
 class EmailAddress {
-  final String value;
+  final Either<ValueFailure<String>, String> value;
 
   factory EmailAddress(String input) {
-    return EmailAddress._(
-      validateEmailAddress(input),
-    );
+    return EmailAddress._(validateEmailAddress(input));
   }
 
   const EmailAddress._(this.value);
@@ -15,23 +15,15 @@ class EmailAddress {
   // toString, equals, hashCode...
 }
 
-String validateEmailAddress(String input) {
-  // Maybe not the most robust way of email validation but it's good enough
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const emailRegex =
       r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
   if (RegExp(emailRegex).hasMatch(input)) {
-    return input;
+    return right(input);
   } else {
-    throw InvalidEmailException(failedValue: input);
+    return left(ValueFailure.invalidEmail(failedValue: input));
   }
 }
-
-class InvalidEmailException implements Exception {
-  final String failedValue;
-
-  InvalidEmailException({required this.failedValue});
-}
-
 
 /*
 void f() {
