21351f9a827b4e6de14684b70ba435d0bdcda16e
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-15 15:14:12 -0300

    Domain and Application classes

--- lib/features/signin/domain/core/value_objects.dart
+++ lib/features/signin/domain/core/value_objects.dart
@@ -13,6 +13,8 @@ abstract class ValueObject<T> {
     return other is ValueObject<T> && other.value == value;
   }
 
+  bool isValid() => value.isRight();
+
   @override
   int get hashCode => value.hashCode;
 
