1c33490eb6186e25ea9a1c6da3c2171b6f92500f
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-18 16:05:30 -0300

    Porra

--- test/features/signin/presentation/pages/sign_in_page_test.dart
+++ test/features/signin/presentation/pages/sign_in_page_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
@@ -12,28 +11,40 @@ import 'package:vethx_beta/features/signin/presentation/cubit/navigation_cubit.d
 import 'package:vethx_beta/features/signin/presentation/routes/sign_in_go_to.dart';
 import 'package:vethx_beta/features/signin/presentation/widgets/sign_in.widgets.dart';
 import 'package:vethx_beta/ui/alpha/alpha.page.dart';
-import 'package:vethx_beta/ui/widgets/shared/progress-indicator.widget.dart';
 
 import 'sign_in_page_test.mocks.dart';
+
+// class MockNavigatorObservererUNSAFE extends Mock implements NavigatorObserver {}
 
 @GenerateMocks([
   AuthBloc,
   SignInBloc,
   NavigationCubit,
-  LoggingNavigationObserver,
+  MaterialPageRoute,
+  NavigatorObserver,
 ])
 void main() {
   late MockAuthBloc _mockAuthBloc;
   late MockSignInBloc _mockSignInBloc;
   late NavigationCubit _navigationCubit;
-  late MockLoggingNavigationObserver _mockLoggingNavigationObserver;
+
+  late MockNavigatorObserverer _mockLoggingNavigationObserver;
+
+  late MockMaterialPageRoute _mockMaterialPageRoute;
+
   late GetIt sl;
 
+  late GlobalKey<NavigatorState> _navigatorKey;
+
   setUp(() {
+    _navigatorKey = GlobalKey<NavigatorState>();
+    _mockMaterialPageRoute = MockMaterialPageRoute();
+
     _mockAuthBloc = MockAuthBloc();
     _mockSignInBloc = MockSignInBloc();
     _navigationCubit = NavigationCubit();
-    _mockLoggingNavigationObserver = MockLoggingNavigationObserver();
+    _mockLoggingNavigationObserver = MockNavigatorObserverer();
+
     sl = GetIt.instance;
     sl.registerFactory<AuthBloc>(() => _mockAuthBloc);
     sl.registerFactory<SignInBloc>(() => _mockSignInBloc);
@@ -47,11 +58,6 @@ void main() {
     when(_mockAuthBloc.stream).thenAnswer((_) => Stream.value(state));
   }
 
-  void _navigationState(NavigationState state) {
-    when(_navigationCubit.state).thenReturn(state);
-    when(_navigationCubit.stream).thenAnswer((_) => Stream.value(state));
-  }
-
   void _signInState(SignInState state) {
     when(_mockSignInBloc.state).thenReturn(state);
     when(_mockSignInBloc.stream).thenAnswer((_) => Stream.value(state));
@@ -60,11 +66,12 @@ void main() {
   Future<void> _pumpWidgetAlphaPage(WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(
+        navigatorKey: _navigatorKey,
         debugShowCheckedModeBanner: false,
         onGenerateRoute: NavigationRoutes.onGenerateRoute,
         routes: NavigationRoutes.routes(),
         // initialRoute: NavigationRoutes.alpha,
-        navigatorObservers: [LoggingNavigationObserver()],
+        navigatorObservers: [_mockLoggingNavigationObserver],
         theme: ThemeData(primarySwatch: Colors.blue),
         home: Builder(builder: (context) {
           return AlphaPage.create(context);
@@ -79,8 +86,6 @@ void main() {
       // Arrange
 
       _authState(const AuthState.unauthenticated());
-
-      _navigationState(const NavigationState.initial());
 
       _signInState(const SignInState.initial());
 
@@ -97,19 +102,38 @@ void main() {
   });
 
   group('when receiving navigation events', () {
-    test('should go to the form password page', () {
-      // arrange
+    testWidgets('should go to the form password page',
+        (WidgetTester tester) async {
+      // Arrange
+
+      _authState(const AuthState.unauthenticated());
+
+      _signInState(const SignInState.initial());
+
+      // Act
+
+      await _pumpWidgetAlphaPage(tester);
 
       // act
+
+      when(_mockLoggingNavigationObserver.navigator)
+          .thenReturn(_navigatorKey.currentState);
 
       _navigationCubit.goTo(SignInPageGoTo(
         from: SignInPageRoutes.registerEmailSignIn,
         to: SignInPageRoutes.passwordEntry,
+        parameters: 'teste@teste.com',
       ));
 
       // assert
 
-      verify(_mockLoggingNavigationObserver.didReplace()).called(1);
+      // verify(_mockLoggingNavigationObserver.didPush(
+      //         _mockMaterialPageRoute, any))
+      //     .called(1);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(FormPassword), findsOneWidget);
     });
   });
 }
