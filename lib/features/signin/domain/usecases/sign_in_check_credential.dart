460ca3abbd78ea56352a2743b9a06a3faf6cc77c
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-21 13:26:49 -0300

    Sign In Check Credential Use Case Unit Tests

--- lib/features/signin/domain/usecases/sign_in_check_credential.dart
+++ lib/features/signin/domain/usecases/sign_in_check_credential.dart
@@ -3,18 +3,19 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:vethx_beta/features/signin/domain/core/failures_details.dart';
 import 'package:vethx_beta/features/signin/domain/core/usecase.dart';
 import 'package:vethx_beta/features/signin/domain/entities/value_objects.dart';
+import 'package:vethx_beta/features/signin/domain/repositories/sign_in_repository.dart';
 import 'package:vethx_beta/features/signin/domain/services/auth_failure.dart';
 import 'package:vethx_beta/features/signin/domain/services/i_auth_facade.dart';
 
 part 'sign_in_check_credential.freezed.dart';
 
 class SignInCredentialCheck extends UseCase<bool, Params> {
-  // final ISignInRepository _signInRepository;
+  final ISignInRepository _signInRepository;
 
   final IAuthFacade _authFacade;
 
   SignInCredentialCheck(
-    // this._signInRepository,
+    this._signInRepository,
     this._authFacade,
   );
 
@@ -23,7 +24,10 @@ class SignInCredentialCheck extends UseCase<bool, Params> {
       _authFacade.credentialIsAlreadyInUse(params.credential).then(
             (value) => value.fold(
               (l) => left(_mapFailures(l)),
-              (r) => right(r),
+              (r) {
+                _signInRepository.cacheCredential(params.credential);
+                return right(r);
+              },
             ),
           );
 
