a9fc2b63030e505ba3c3324f7f2a833a7e60947c
Valter Barbosa <contato@valterbarbosa.com.br>
2021-08-24 11:55:01 -0300

    Integration Tests Infrastructure

--- integration_test/foo_test.dart
+++ integration_test/foo_test.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:vethx_beta/features/signin/presentation/pages/sign_in_credential.page.dart';
 import 'package:vethx_beta/features/signin/presentation/widgets/sign_in.widgets.dart';
 
 import 'package:vethx_beta/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  final binding = IntegrationTestWidgetsFlutterBinding();
 
   group('end-to-end test', () {
     testWidgets('tap on the sign in with credential button; verify button',
@@ -22,7 +25,28 @@ void main() {
 
       await tester.pumpAndSettle();
 
-      expect(signInWithCredentials, findsOneWidget);
+      expect(find.byType(SignInCredentialPage), findsOneWidget);
+    });
+
+    testWidgets('tap on the sign in with credential button; verify button',
+        (WidgetTester tester) async {
+      app.main();
+
+      // This is required prior to taking the screenshot (Android only).
+      await binding.convertFlutterSurfaceToImage();
+
+      await tester.pumpAndSettle();
+
+      final signInWithCredentials =
+          find.byKey(const Key(SignInPageKeys.signInWithCredential));
+
+      await tester.tap(signInWithCredentials);
+
+      await tester.pumpAndSettle();
+
+      await binding.takeScreenshot('screenshot-1');
+
+      expect(find.byType(SignInCredentialPage), findsOneWidget);
     });
   });
 }
@@ -35,3 +59,9 @@ void main() {
 //     expect(2 + 2, equals(5));
 //   });
 // }
+
+/**
+ * 
+flutter drive --driver=test_driver/integration_test_driver_extended.dart \
+--target=integration_test/foo_test.dart
+ */
--- integration_test/integration_test_driver.dart
+++
@@ -1,9 +0,0 @@
-import 'package:integration_test/integration_test_driver.dart';
-
-Future<void> main() => integrationDriver();
-
-/**
- * 
-flutter drive --driver=integration_test/integration_test_driver.dart \
---target=test_driver/foo_test.dart
- */
